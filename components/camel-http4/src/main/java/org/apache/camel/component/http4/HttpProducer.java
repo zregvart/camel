df954e414828894728a542ffefcf34ce88279490
Willem Jiang <willem.jiang@gmail.com>
2014-12-22 08:56:13 +0800

    [ENTESB-2344] Camel Jetty/Http4 producers should respect Content-Length/Transfer-Encoding:Chunked headers
    Cherry-picked from e0cea4e804487b616161c7b9a7385ba5e3133f28
    Modified JUnit test

--- components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpProducer.java
+++ components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpProducer.java
@@ -180,7 +180,7 @@ public class HttpProducer extends DefaultProducer {
                 httpRequest.setHeader("Host", hostHeader);
             }
         }
-        
+
         if (getEndpoint().isConnectionClose()) {
             httpRequest.addHeader("Connection", HTTP.CONN_CLOSE);
         }
@@ -560,9 +560,13 @@ public class HttpProducer extends DefaultProducer {
                     if (answer == null) {
                         // force the body as an input stream since this is the fallback
                         InputStream is = in.getMandatoryBody(InputStream.class);
-                        
-                        InputStreamEntity entity = new InputStreamEntity(is, -1);
-                        
+                        String length = in.getHeader(Exchange.CONTENT_LENGTH, String.class);
+                        InputStreamEntity entity = null;
+                        if (ObjectHelper.isEmpty(length)) {
+                            entity = new InputStreamEntity(is, -1);
+                        } else {
+                            entity = new InputStreamEntity(is, Long.parseLong(length));
+                        }
                         if (contentType != null) {
                             entity.setContentType(contentType.toString());
                         }
