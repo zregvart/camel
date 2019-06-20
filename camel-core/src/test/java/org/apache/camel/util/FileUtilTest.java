88b248b80b78359e7463c29f3cf458624cc2521a
cblamauer <52042437+cblamauer@users.noreply.github.com>
2019-06-20 17:21:52 +0200

    CAMEL-13667
    Windows network UNC paths not treated correctly (File2/tempPrefix) test
    (cherry picked from commit ef10fb86abe909f85002d3a2c7215dccf8aafb6d)

--- camel-core/src/test/java/org/apache/camel/util/FileUtilTest.java
+++ camel-core/src/test/java/org/apache/camel/util/FileUtilTest.java
@@ -158,6 +158,8 @@ public class FileUtilTest extends TestCase {
             assertEquals("foo\\bar\\baz", FileUtil.compactPath("foo\\bar\\.\\baz"));
             assertEquals("foo\\bar\\baz", FileUtil.compactPath("foo\\bar\\\\baz"));
             assertEquals("\\foo\\bar\\baz", FileUtil.compactPath("\\foo\\bar\\baz"));
+            // Test that multiple back-slashes at the beginning are preserved, this is necessary for network UNC paths.
+            assertEquals("\\\\foo\\bar\\baz", FileUtil.compactPath("\\\\foo\\bar\\baz"));
             assertEquals("\\", FileUtil.compactPath("\\"));
             assertEquals("\\", FileUtil.compactPath("/"));
             assertEquals("/", FileUtil.compactPath("\\", '/'));
@@ -177,6 +179,8 @@ public class FileUtilTest extends TestCase {
             assertEquals("foo/bar/baz", FileUtil.compactPath("foo/bar/./baz"));
             assertEquals("foo/bar/baz", FileUtil.compactPath("foo/bar//baz"));
             assertEquals("/foo/bar/baz", FileUtil.compactPath("/foo/bar/baz"));
+            // Do not preserve multiple slashes at the beginning if not on Windows.
+            assertEquals("/foo/bar/baz", FileUtil.compactPath("//foo/bar/baz"));
             assertEquals("/", FileUtil.compactPath("/"));
             assertEquals("/", FileUtil.compactPath("\\"));
             assertEquals("/", FileUtil.compactPath("/", '/'));
