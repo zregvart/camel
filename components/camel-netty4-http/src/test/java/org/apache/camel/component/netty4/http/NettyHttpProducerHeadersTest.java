d03a945713a49c331d08ca9d8ae904233eab1ee6
Jan Bednář <mail@janbednar.eu>
2019-07-05 15:45:52 +0200

    CAMEL-13707: checkstyle
    (cherry picked from commit 60946487b6ecb16d7fc1a31af4a060c3acd97f79)
    
    # Conflicts:
    #	components/camel-netty4-http/src/test/java/org/apache/camel/component/netty4/http/NettyHttpProducerHeadersTest.java

--- components/camel-netty4-http/src/test/java/org/apache/camel/component/netty4/http/NettyHttpProducerHeadersTest.java
+++ components/camel-netty4-http/src/test/java/org/apache/camel/component/netty4/http/NettyHttpProducerHeadersTest.java
@@ -77,7 +77,7 @@ public class NettyHttpProducerHeadersTest extends CamelTestSupport {
                 port = AvailablePortFinder.getNextAvailable(8000);
 
                 from("netty4-http:http://localhost:" + port + "?matchOnUriPrefix=true")
-                    .setBody(simple("${header."+Exchange.HTTP_URI+"}"));
+                    .setBody(simple("${header." + Exchange.HTTP_URI + "}"));
             }
         };
     }
