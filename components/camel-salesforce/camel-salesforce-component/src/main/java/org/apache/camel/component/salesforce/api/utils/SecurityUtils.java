f217e35e230537b9a9b1a781893a9e73522ce50f
JiriOndrusek <jondruse@redhat.com>
2019-11-01 14:25:16 +0100

    [ENTESB-7464] Camel salesforce does not run on IBM java

---
+++ components/camel-salesforce/camel-salesforce-component/src/main/java/org/apache/camel/component/salesforce/api/utils/SecurityUtils.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.salesforce.api.utils;
+
+import java.util.Arrays;
+
+import org.apache.camel.util.jsse.SSLContextParameters;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+
+public final class SecurityUtils {
+
+    private SecurityUtils() {
+    }
+
+    public static void adaptToIBMCipherNames(final SslContextFactory sslContextFactory) {
+        //jetty client adds into excluded cipher suites all ciphers starting with SSL_
+        //it makes sense for Oracle jdk, but in IBM jdk all ciphers starts with SSL_, even ciphers for TLS
+        //see https://github.com/eclipse/jetty.project/issues/2921
+        if (System.getProperty("java.vendor").contains("IBM")) {
+            String[] excludedCiphersWithoutSSLExclusion = Arrays.stream(sslContextFactory.getExcludeCipherSuites())
+                    .filter(cipher -> !cipher.equals("^SSL_.*$"))
+                    .toArray(String[]::new);
+            sslContextFactory.setExcludeCipherSuites(excludedCiphersWithoutSSLExclusion);
+        }
+    }
+
+    public static void adaptToIBMTlsVersion(final SSLContextParameters sslContextParameters) {
+        //salesforce requires tls1.2. Oracle jdk is by default accepting such connections, but IBM java works by default only with TLS1
+        // and this is a way to force it to use TLS1.2 (or to use -Dcom.ibm.jsse2.overrideDefaultTLS=true for jdk)
+        if (System.getProperty("java.vendor").contains("IBM")) {
+            sslContextParameters.setSecureSocketProtocol("TLSv1.2");
+        }
+    }
+}
